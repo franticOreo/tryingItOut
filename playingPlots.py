a32085361223eb34bd0fd9ab05eb02ffd41c2d1b
franticOreo <eli.simic.robertson@gmail.com>
2018-08-15 11:03:05 +0000

    adding input for initial games
    green table

---
+++ playingPlots.py
@@ -0,0 +1,31 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+import matplotlib.pyplot as pyplot
+
+
+print("This is console module")
+
+
+
+spins = []
+game1_result = [8,24,32,24,33,19,5,12,14,32,7,22,5,8,6,28,32,23
+        ,5,8,6,28,32,23,4,32,27,19,31,9,19,13]
+
+game2_result = [36,5,20,7,18,13,7,4,28,2,36,34,
+        34,32,19,33,27,31]
+
+
+
+for i in range(1,33):
+    spins.append(i)
+
+
+
+print(len(result) == len(spins))
+
+
+#print(len(result))
+#print(len([1]))
+#print(spins)
